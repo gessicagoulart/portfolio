c6e91cf6ea1d4c00fc2e2c5501f9f45be070c94b
gessica mariana Goulart <gessica_mariana12@hotmail.com>
2020-09-23 11:55:45 +0100

    Changed some color details

--- main.js
+++ main.js
@@ -23,6 +23,11 @@ $(document).ready(function(){
 
     AOS.init({
         easing: 'ease',
-        duration: 1800
+        duration: 1800,
+        once: true
     })
+    if ($(window).scrollTop() > navHeight) {
+
+        $('.nav-link').addClass('fixed');
+    }
 });
